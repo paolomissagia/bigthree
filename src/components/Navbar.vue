cfe006e3fa0a2b851205eede4ed274a0fffcea80
Paulo Cesar Missagia Filho <missagiafilhop@emea.cshare.net>
2023-08-17 00:51:30 +0100

    updated navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -5,58 +5,11 @@
     <div
       class="container px-4 mx-auto flex flex-wrap items-center justify-between"
     >
-      <div
-        class="w-full relative flex justify-between lg:w-auto lg:static lg:block lg:justify-start"
+      <span
+        class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
+        >Big Three</span
       >
-        <a
-          class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white"
-          href="https://www.creative-tim.com/learning-lab/tailwind-starter-kit#/presentation"
-          >Big Three</a
-        ><button
-          class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none"
-          type="button"
-          v-on:click="toggleNavbar()"
-        >
-          <i class="text-white fas fa-bars"></i>
-        </button>
-      </div>
-      <div
-        class="lg:flex flex-grow items-center bg-white lg:bg-transparent lg:shadow-none"
-        v-bind:class="{ hidden: !showMenu, block: showMenu }"
-      >
-        <ul class="flex flex-col lg:flex-row list-none lg:ml-auto">
-          <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#pablo"
-              ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-facebook text-lg leading-lg"
-              ></i
-              ><span class="lg:hidden inline-block ml-2">Share</span></a
-            >
-          </li>
-          <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#pablo"
-              ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-twitter text-lg leading-lg"
-              ></i
-              ><span class="lg:hidden inline-block ml-2">Tweet</span></a
-            >
-          </li>
-          <li class="flex items-center">
-            <a
-              class="lg:text-white lg:hover:text-gray-300 text-gray-800 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-              href="#pablo"
-              ><i
-                class="lg:text-gray-300 text-gray-500 fab fa-github text-lg leading-lg"
-              ></i
-              ><span class="lg:hidden inline-block ml-2">Star</span></a
-            >
-          </li>
-        </ul>
-      </div>
+      <span><i class="text-white fas fa-trophy"></i></span>
     </div>
   </nav>
 </template>
@@ -64,15 +17,5 @@
 <script>
 export default {
   name: "NavbarComponent",
-  data() {
-    return {
-      showMenu: false,
-    };
-  },
-  methods: {
-    toggleNavbar: function () {
-      this.showMenu = !this.showMenu;
-    },
-  },
 };
 </script>
