3d8d9b627fad3913879362b5b7b765d8da302e82
Paulo Cesar Missagia Filho <missagiafilhop@emea.cshare.net>
2023-08-16 18:58:33 +0100

    updated root component to reflect new hero page

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HeroPage />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeroPage from "./pages/HeroPage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HeroPage,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@tailwind base;
+@tailwind components;
+@tailwind utilities;
 </style>
