942285f08a2b928907fe193e954e9d30db531dc0
Paulo Cesar Missagia Filho <missagiafilhop@emea.cshare.net>
2023-08-17 12:25:29 +0100

    updated root component of the project

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,29 @@
 <template>
+  <navbar-component />
   <HeroView />
+  <PlayersView />
+  <SlamsView />
+  <MastersView />
+  <footer-component />
 </template>
 
 <script>
+import NavbarComponent from "./components/Navbar.vue";
 import HeroView from "./views/Hero.vue";
+import PlayersView from "./views/Players.vue";
+import SlamsView from "./views/Slams.vue";
+import MastersView from "./views/Masters.vue";
+import FooterComponent from "./components/Footer.vue";
 
 export default {
   name: "App",
   components: {
+    NavbarComponent,
     HeroView,
+    PlayersView,
+    SlamsView,
+    MastersView,
+    FooterComponent,
   },
 };
 </script>
