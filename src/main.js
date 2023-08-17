f1b28620f23caf0252e87b23972a488785753fe6
Paulo Cesar Missagia Filho <missagiafilhop@emea.cshare.net>
2023-08-17 10:20:04 +0100

    added vue auto counter

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import Vue3Autocounter from "vue3-autocounter";
 
-createApp(App).mount("#app");
+createApp(App).component("vue3-autocounter", Vue3Autocounter).mount("#app");
